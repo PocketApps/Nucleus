31c2a86a3768b5899580cbb8f424abb0693b622b
Henry Jooste <henry.jooste@seecrypt.com>
2016-03-10 21:19:39 +0200

    Updated examples

--- examples/complete.php
+++ examples/complete.php
@@ -10,13 +10,31 @@ nucleus::init_styled(array('css/bootstrap.min.css', 'css/custom.css'));
 nucleus::network_tests("Your Website Name", "https://www.example.com");
 
 nucleus::api_test("Test API", "This API is only being used to test the Nucleus testing framework", array(
-    nucleus::create_test("Using the correct password", "https://www.example.com/authentication.php?password=testing", "error", false),
-    nucleus::create_test("Using an incorrect password", "https://www.example.com/authentication.php?password=123456", "error", true),
-    nucleus::create_test("Without providing a password", "https://www.example.com/authentication.php", "error", true)
+    nucleus::create_test("Using the correct password", "https://www.example.com/authentication.php", array(
+        "password" => "testing"
+    ), "error", false, "GET"),
+    nucleus::create_test("Using an incorrect password", "https://www.example.com/authentication.php", array(
+        "password" => "123456"
+    ), "error", true, "GET"),
+    nucleus::create_test("Without providing a password", "https://www.example.com/authentication.php", "", "error", true, "GET")
 ));
 
 nucleus::api_test("Test API (With Bug)", "This API is only being used to test the Nucleus testing framework", array(
-    nucleus::create_test("Using the correct password", "https://www.example.com/authentication_bug.php?password=testing", "error", false),
-    nucleus::create_test("Using an incorrect password", "https://www.example.com/authentication_bug.php?password=123456", "error", true),
-    nucleus::create_test("Without providing a password", "https://www.example.com/authentication_bug.php", "error", true)
+    nucleus::create_test("Using the correct password", "https://www.example.com/authentication_bug.php", array(
+        "password" => "testing"
+    ), "error", false, "GET"),
+    nucleus::create_test("Using an incorrect password", "https://www.example.com/authentication_bug.php", array(
+        "password" => "123456"
+    ), "error", true, "GET"),
+    nucleus::create_test("Without providing a password", "https://www.example.com/authentication_bug.php", "", "error", true, "GET")
+));
+
+nucleus::api_test("Test POST API", "This API is only being used to test the Nucleus testing framework", array(
+    nucleus::create_test("Using the correct password", "https://www.example.com/authentication_post.php", array(
+        "password" => "testing"
+    ), "error", false, "POST"),
+    nucleus::create_test("Using an incorrect password", "https://www.example.com/authentication_post.php", array(
+        "password" => "123456"
+    ), "error", true, "POST"),
+    nucleus::create_test("Without providing a password", "https://www.example.com/authentication_post.php", "", "error", true, "POST")
 ));
